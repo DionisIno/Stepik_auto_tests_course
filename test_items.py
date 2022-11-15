7dcf931ce5b07b73f4e85bf1ee03b40ab6dbb8c8
DionisIno <109044090+DionisIno@users.noreply.github.com>
2022-11-16 00:58:34 +0300

    Add files via upload

--- test_items.py
+++ test_items.py
@@ -1,11 +1,19 @@
 import time
 
-
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_button_add_to_cart(browser):
-  browser.get(link)
-  # time.sleep(30)
-  button = browser.find_element_by_css_selector('button.btn-add-to-basket').get_attribute('value')
-  assert len(button) > 0, 'Add to cart button not found. (Кнопка добавления в корзину не найдена)'
+class TestProductPage:
+
+    def test_button_add_to_basket_is_visible(self, browser):
+        """Тест проверяет, что страница товара
+     содержит кнопку добавления в корзину
+    """
+
+        browser.get(link)
+
+
+        time.sleep(5)
+
+
+        assert browser.find_element_by_css_selector("button.btn-add-to-basket")
