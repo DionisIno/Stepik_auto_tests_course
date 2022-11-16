cd936a2b94139b33bf4dca9b8b0d9517c1d47508
DionisIno <109044090+DionisIno@users.noreply.github.com>
2022-11-16 17:03:36 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,19 +1,10 @@
 import time
+from selenium.webdriver.common.by import By
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-
-
-class TestProductPage:
-
-    def test_button_add_to_basket_is_visible(self, browser):
-        """Тест проверяет, что страница товара
-     содержит кнопку добавления в корзину
-    """
-
-        browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-        time.sleep(5)
-
-
-        assert browser.find_element_by_css_selector("button.btn-add-to-basket")
+def test_button_add_to_basket(browser):
+     browser.get(link)
+     time.sleep(5)
+     assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
